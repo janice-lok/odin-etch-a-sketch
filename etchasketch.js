b88906d8763394be53fe5e9d4de50185e9a1a710
Janice Lok <160542635+janice-lok@users.noreply.github.com>
2024-05-02 16:30:44 -0400

    Add and modify etchasketch.css etchasketch.html etchasketch.js to create a webpage with 16x16 square grid

---
+++ etchasketch.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {      // 16 rows
+    for (let j = 0; j < 16; j++) {      // 16 columns
+        const squareDiv = document.createElement("div");
+        squareDiv.style.backgroundColor = "lavenderblush";
+        squareDiv.style.border = "0.05px solid palevioletred";
+        container.appendChild(squareDiv);
+
+        squareDiv.addEventListener("mouseover", () => {
+            squareDiv.style.backgroundColor = "palevioletred";
+        });
+
+    };
+};
